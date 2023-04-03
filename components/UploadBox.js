1fe11391d0c0d312636430175663a63815d8d47f
Hanish Jindal <hanish1139.cse19@chitkara.edu.in>
2023-04-03 12:37:37 +0530

    V3.0 Responsive

--- components/UploadBox.js
+++ components/UploadBox.js
@@ -22,7 +22,7 @@ function UploadBox({
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   return (
     <div
-      className={`uploadBox bg-neutral-800 m-5 rounded-3xl flex flex-col justify-center items-center text-lg border-2 border-dashed w-[600px] h-[350px] duration-300 ${
+      className={`uploadBox bg-neutral-800 m-5 rounded-3xl flex flex-col justify-center items-center text-lg border-2 border-dashed md:w-[40vw] md:p-0 h-[350px] duration-300 w-80vw ${
         isDragActive && "border-green-600"
       }`}
       {...getRootProps()}
@@ -30,21 +30,21 @@ function UploadBox({
       {/* <input {...getInputProps()} /> */}
       {selectedFile && preview && gif && (
         <Image
-          className="preview mb-7 rounded-3xl m-1 w-[380px] h-[230px] absolute"
+          className="preview mb-10 rounded-3xl md:w-[370px] h-[230px] absolute w-[70%]"
           src={preview}
           alt=""
-          width={350}
+          width={250}
           height={230}
         />
       )}
       {showUpload ? (
         <div>
           {isDragActive ? (
-            <p className={`${selectedFile && "mt-[270px]"}`}>
+            <p className={`mx-4 text-center ${selectedFile && "mt-[270px]"}`}>
               Drop the image here
             </p>
           ) : (
-            <p className={`${selectedFile && "mt-[270px]"}`}>
+            <p className={`mx-4 text-center ${selectedFile && "mt-[270px]"}`}>
               Drag and drop your image here or click to select files
             </p>
           )}
@@ -55,10 +55,10 @@ function UploadBox({
             <Loader />
           ) : (
             <Image
-              className="preview rounded-3xl h-[350px]"
+              className="preview rounded-3xl h-[350px] md:w-[100%] w-[80vw]"
               src={link}
               alt=""
-              width={800}
+              width={400}
               height={250}
             />
           )}
